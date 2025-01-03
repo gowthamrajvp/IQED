cec72da0e3b79917667e4f13498cf03c2c7191d7
gowthamrajvp <gowthamraj9344@gmail.com>
2025-01-03 12:10:27 +0530

    F.1

--- src/Route/Routes.jsx
+++ src/Route/Routes.jsx
@@ -31,37 +31,6 @@ export const Routers = createBrowserRouter(
           </Route>
         </Route>
       </Route>
-      {/* <Route path="auth" element={<AuthPage />} /> */}
-      {/* <Route element={<AuthLayout />}>
-        <Route element={<UserLayout />}>
-          <Route path="explore" element={<ExplorePage />} />
-          <Route path="missions" element={<MissionPage />} />
-          <Route path="game" element={<GamePage />} />
-          <Route path="Profile" element={<ProfilePage />} />
-          <Route path="Leaderboard" element={<LeaderboardPage />} />
-          <Route path="FeedBack" element={<FeedBackPage />} />
-          <Route path="store" element={<Outlet />}>
-            <Route index element={<StorePage />} />
-            <Route path="shipping/:productId" element={<Shipping />} />
-            <Route path="Orders" element={<StoreOrderLayout />}>
-              <Route index element={<YourOrders />} />
-              <Route path=":orderId" element={<OrderDetail />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route path="quiz" element={<Outlet />}>
-          <Route path=":sessionId" element={<QuizLayout />}>
-            <Route index element={<QuizPage />} />
-          </Route>
-        </Route>
-      </Route>
-      <Route path="match" element={<MatchLayout />}>
-        <Route path=":GameCode" element={<Outlet />}>
-          <Route index element={<MatchLobby />} loader={OnLoadLobby} />
-          <Route path=":GameSessionId" element={<MPQuizPage />} />3w
-        </Route>
-      </Route> */}
-
       <Route path="*" element={<PageNotFound />} />
     </Route>
 
